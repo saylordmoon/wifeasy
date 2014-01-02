2a315168c75f6bd6272e87659da376061f37c93b
saylordmoon <saylordmoon@gmail.com>
2014-01-02 16:10:44 +0000

    Ideas

--- wifeasy.py
+++ wifeasy.py
@@ -3,11 +3,12 @@ import sys
 import subprocess
 import re
 
-CMD_GET_INTERFACE   = "iw dev | grep Interface"
-CMD_START_INTERFACE = "ip link set wls32 up"
+CMD_INTERFACES      = "iw dev | grep Interface"
+CMD_START_INTERFACE = "ip link set {0} up"
 CMD_CONNECT_WEP     = "iw dev {0} connect {1} key 0:{2}"
-CMD_SCAN_NETWORKS   = "sudo iw dev wls32 scan | grep SSID:"
+CMD_SCAN_NETWORKS   = "sudo iw dev {0} scan | grep SSID:"
 RE_INTERFACES       = "Interface\s+(\w+)"
+RE_SSID             = "SSID:\s+(\w+)"
 
 def getInterface(index=None):
     if index==None:
@@ -17,27 +18,38 @@ def getInterface(index=None):
         return interfaces[index]
 
 def getIntefaces():
-    interfaces = subprocess.check_output([CMD_GET_INTERFACE],shell=True)
+    interfaces = subprocess.check_output([CMD_INTERFACES],shell=True)
     interfaces = re.findall(RE_INTERFACES,interfaces)
     return interfaces
 
-def connect(network,password,interface=None,networkType=None):
-	if networkType == None:
-		networkType = "wep"
+def getNetworks(interface=None):
     if interface == None:
         interface=getInterface()
-	return getConnectCommand(interface,network,password)
+    networks = subprocess.check_output([getScanCmd(interface)])
+    networks = re.findall(RE_INTERFACES,networks)
+    return networks
 
-def getConnectCommand(interface,network,key):
+def connect(network,password,interface=None,networkType=None):
+    if networkType == None:
+        networkType = "wep"
+    if interface == None:
+        interface=getInterface()
+    return getConnectCmd(network,password,interface)
+
+def getStartInterfaceCmd(interface):
+    return CMD_START_INTERFACE.format(interface)
+
+def getConnectCmd(network,key,interface):
     return CMD_CONNECT_WEP.format(interface,network,key)
 
-    
-def main():
+def getScanCmd(interface):
+    return CMD_SCAN_NETWORKS.format(interface)
 
-    # if len(sys.argv) >= 2:
-    #     network = sys.argv[1]
-    # if len(sys.argv) >= 3:
-    #     password = sys.argv[2]
+def main():
+    if len(sys.argv) >= 2:
+        network = sys.argv[1]
+    if len(sys.argv) >= 3:
+        password = sys.argv[2]
     print connect(network,password)
 
 if __name__ == '__main__':
